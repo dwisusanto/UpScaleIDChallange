8c8e16446d18a66c229d021fd322b81d08cc5ed1
Dwi Susanto <kansertjr@gmail.com>
2023-03-16 06:01:45 +0700

    update after test API

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mysql = require('mysql');
+// server.js
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 
@@ -9,59 +11,69 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'task_manager'
+  host: "localhost",
+  user: "root",
+  password: "12345678",
+  database: "upscaleid",
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL database: ' + err.stack);
+    console.error("Error connecting to MySQL database: " + err.stack);
     return;
   }
-  console.log('Connected to MySQL database with connection id ' + connection.threadId);
+  console.log("Connected to MySQL database with connection id " + connection.threadId);
 });
 
-app.get('/tasks', (req, res) => {
-  const sql = 'SELECT * FROM tasks';
+app.get("/tasks", (req, res) => {
+  const sql = "SELECT * FROM tasks";
   connection.query(sql, (error, results) => {
     if (error) throw error;
     res.send(results);
   });
 });
 
-app.post('/task', (req, res) => {
+app.post("/tasks", (req, res) => {
   const { title, description, completed } = req.body;
-  const sql = 'INSERT INTO tasks (title, description, completed) VALUES (?, ?, ?)';
+  const sql = "INSERT INTO tasks (title, description, completed) VALUES (?, ?, ?)";
   connection.query(sql, [title, description, completed], (error, results) => {
     if (error) throw error;
     res.send(results);
   });
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get("/tasks/:id", (req, res) => {
   const id = req.params.id;
-  const sql = 'SELECT * FROM tasks WHERE id = ?';
+  const sql = "SELECT * FROM tasks WHERE task_id = ?";
   connection.query(sql, [id], (error, results) => {
     if (error) throw error;
     res.send(results[0]);
   });
 });
 
-app.patch('/tasks/:id', (req, res) => {
+app.patch("/tasks/:id", (req, res) => {
   const id = req.params.id;
   const { title, description, completed } = req.body;
-  const sql = 'UPDATE tasks SET title = ?, description = ?, completed = ? WHERE id = ?';
+  const sql = "UPDATE tasks SET title = ?, description = ?, completed = ? WHERE task_id = ?";
   connection.query(sql, [title, description, completed, id], (error, results) => {
     if (error) throw error;
     res.send(results);
   });
 });
 
-app.delete('/tasks/:id', (req, res) => {
+app.delete("/tasks/:id", (req, res) => {
   const id = req.params.id;
-  const sql = 'DELETE FROM tasks WHERE id = ?';
+  const sql = "DELETE FROM tasks WHERE task_id = ?";
   connection.query(sql, [id], (error, results) => {
     if (error) throw error;
-    res.send(results
+    res.send(results);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to simple task API");
+});
+
+app.listen(4000, () => {
+  console.log("Listening to port 4000");
+});
